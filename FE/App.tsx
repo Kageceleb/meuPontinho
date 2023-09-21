e2495c970f3c57b4d10a009e995d369e7a4fafdf
Samuel Sant Anna <samuelsantaanna@gmail.com>
2023-09-21 19:47:32 -0300

    FE development

--- FE/App.tsx
+++ FE/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { Alert, Button, Text, View } from 'react-native';
 import { styles } from './styles';
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchHeadingAsync, watchPositionAsync, LocationAccuracy} from 'expo-location'
 import MapView, {Marker} from 'react-native-maps';
@@ -39,7 +39,12 @@ export default function App() {
   },[])
 
   return (
+
     <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.info}>Meu Pontinho</Text>
+      <Text style={styles.text}>Nome da Disciplina</Text>
+      <Text style={styles.text}>Nome do Aluno da Silva Soares</Text>
      {location &&
       <MapView
       ref={mapRef}
@@ -59,7 +64,13 @@ export default function App() {
       }}
            />
       </MapView>}
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Entrada</Text>
+      <Text style={styles.text}>Referência: 08:00</Text>
+      <Text style={styles.text}>Contador (cronometro)</Text>
+      <Button color='#6a5acd' title='Entrada'  onPress={() => Alert.alert('Simple Button pressed')}/>
+      <Text style={styles.text}></Text>
+
     </View>
+    
   );
 }
--- FE/styles.ts
+++ FE/styles.ts
@@ -11,5 +11,27 @@ export const styles = StyleSheet.create({
       flex: 1,
       width:'100%'
     },
+    info:{
+      flexDirection:'column',
+      width:'100%',
+      height:75,
+      alignItems:'center',
+      textAlign:'center',
+      fontWeight: 'bold',
+      textAlignVertical:'bottom',
+      backgroundColor:'#6a5acd',
+      fontSize:25,
+    },
+    text:{
+      flexDirection:'column',
+      width:'100%',
+      height:35,
+      alignItems:'center',
+      textAlign:'center',
+      fontWeight: 'bold',
+      textAlignVertical:'bottom',
+      backgroundColor:'#708090',
+      fontSize:20,
+    },
   });
   
